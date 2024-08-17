5c9f85ec47793ebab1b0f3063ee8e7ea64694de4
reoring <reoring@gmail.com>
2024-08-17 15:49:30 +0900

    chore: remove InfraClusterSecretRef

--- api/v1alpha1/hnmachine_types.go
+++ api/v1alpha1/hnmachine_types.go
@@ -1,19 +1,3 @@
-/*
-Copyright 2024.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package v1alpha1
 
 import (
@@ -50,9 +34,8 @@ type HnMachineSpec struct {
 	// +optional
 	BootstrapCheckSpec BootstrapCheckSpec `json:"bootstrapCheckSpec,omitempty"`
 
-	// InfraClusterSecretRef is a reference to a secret with credentials for the infrastructure where the container will run.
-	// +optional
-	InfraClusterSecretRef *corev1.ObjectReference `json:"infraClusterSecretRef,omitempty"`
+	// InfraClusterSecretRef field has been removed as it is no longer needed.
+	// HnMachine operates only within Kubernetes Pods and does not interact directly with external infrastructure.
 }
 
 // HnMachineContainerTemplate defines the core settings for the HnMachine container
